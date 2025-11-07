30fe26658a58f3c336070c79a7ec8c34f79c1bd9
DaK-code <dariusgavi124@gmail.com>
2025-11-07 10:31:42 +0000

    Added remove_task() feature

--- main.py
+++ main.py
@@ -13,3 +13,10 @@ if __name__ == "__main__":
     add_task("Setup repository")
     add_task("Write documentation")
     show_tasks()
+
+def remove_task(index):
+    if 0 <= index < len(tasks):
+        removed = tasks.pop(index)
+        print(f"🗑️ Removed : {removed}")
+    else:
+        print("⚠️ Invalid index")
